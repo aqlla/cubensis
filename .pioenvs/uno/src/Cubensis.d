521db6899703d47f4f7634a2a1bd58530a088afe
Aquilla Sherrock <acsherrock@gmail.com>
2015-06-01 09:13:56 -0500

    rolled back to help resolve merge conflict

---
+++ .pioenvs/uno/src/Cubensis.d
@@ -0,0 +1,16 @@
+.pioenvs/uno/src/Cubensis.o: src/Cubensis.cpp src/Cubensis.h \
+ .pioenvs/uno/FrameworkArduino/Arduino.h \
+ .pioenvs/uno/FrameworkArduino/binary.h \
+ .pioenvs/uno/FrameworkArduino/WCharacter.h \
+ .pioenvs/uno/FrameworkArduino/WString.h \
+ .pioenvs/uno/FrameworkArduino/HardwareSerial.h \
+ .pioenvs/uno/FrameworkArduino/Stream.h \
+ .pioenvs/uno/FrameworkArduino/Print.h \
+ .pioenvs/uno/FrameworkArduino/Printable.h \
+ .pioenvs/uno/FrameworkArduino/USBAPI.h \
+ .pioenvs/uno/FrameworkArduino/Arduino.h \
+ .pioenvs/uno/FrameworkArduinoVariant/pins_arduino.h \
+ .pioenvs/uno/Servo/Servo.h .pioenvs/uno/Servo/avr/ServoTimers.h \
+ src/motor.h src/itvec.h src/pid.h src/imu.h \
+ .pioenvs/uno/mpu6050/MPU6050.h .pioenvs/uno/i2cdev/I2Cdev.h \
+ .pioenvs/uno/Wire/Wire.h .pioenvs/uno/FrameworkArduino/Stream.h
