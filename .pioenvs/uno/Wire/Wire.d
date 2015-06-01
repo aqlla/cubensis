8119a8fe21107df8c332ed897acd925bea6d5ef6
Aquilla Sherrock <acsherrock@gmail.com>
2015-06-01 09:05:21 -0500

    ignoring .pioenvs/ directory

--- .pioenvs/uno/Wire/Wire.d
+++
@@ -1,6 +0,0 @@
-.pioenvs/uno/Wire/Wire.o: .pioenvs/uno/Wire/Wire.cpp \
- .pioenvs/uno/Wire/utility/twi.h .pioenvs/uno/Wire/Wire.h \
- .pioenvs/uno/FrameworkArduino/Stream.h \
- .pioenvs/uno/FrameworkArduino/Print.h \
- .pioenvs/uno/FrameworkArduino/WString.h \
- .pioenvs/uno/FrameworkArduino/Printable.h
--- .pioenvs/uno/Wire/utility/twi.d
+++
@@ -1,5 +0,0 @@
-.pioenvs/uno/Wire/utility/twi.o: .pioenvs/uno/Wire/utility/twi.c \
- .pioenvs/uno/FrameworkArduino/Arduino.h \
- .pioenvs/uno/FrameworkArduino/binary.h \
- .pioenvs/uno/FrameworkArduinoVariant/pins_arduino.h \
- .pioenvs/uno/Wire/utility/twi.h
